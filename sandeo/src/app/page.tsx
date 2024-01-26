2e67c78fff5d21c1fabbb6698498a070602031e5
sebastienambona <sebastien.ambona@agence-gw.com>
2024-01-26 20:50:08 +0100

    WIP: Work on design connect page

--- sandeo/src/app/page.tsx
+++ sandeo/src/app/page.tsx
@@ -8,12 +8,17 @@ import "./globals.css";
 
 export default function Page() {
   return (
-      <div className="flex h-screen ">
-          <div className="leftPart w-11/12 bg-secondary">
+      <div className="flex h-screen bg-secondary">
+          <div className="formLogin  bg-white rounded-lg flex-col justify-center items-center align m-auto w-4/12 p-3">
 
-          </div>
-          <div className="rightPart flex-col justify-center items-center align h-full w-6/12">
-              <h1 className='text-4xl'>Sandeo</h1>
+              <div className="formHeader flex justify-around ">
+                  <h1 className='text-6xl pt-5 titleWebsite'>Sandeo</h1>
+                  <img src='/logoSandeo.svg' alt='logo application' className='w-16'/>
+              </div>
+              <div className="buttonChangeForm flex justify-center mt-6">
+                  <button className='p-1 border-2 border-secondary rounded-tl-lg rounded-bl-lg'>Connexion</button>
+                  <button className='p-1 border-2 border-secondary rounded-tr-lg rounded-br-lg'>Inscription</button>
+              </div>
               <div className="hidden"><Form/></div>
               <FormConnection/>
           </div>
