c6232be84b617329db10a8abfd3d832773c94231
Ambona Sebastien <sebastien.ambona@agence-gw.com>
2024-01-30 19:53:24 +0100

    connection_BackendFront: Add Connect routes and make modification in front for create account

--- sandeo/src/app/components/Login/FormCreateAccount.client.tsx
+++ sandeo/src/app/components/Login/FormCreateAccount.client.tsx
@@ -1,20 +1,51 @@
 // src/components/Login/FormCreateAccount.client.tsx
 "use client";
 import React from "react";
+import bcrypt from "bcrypt";
 
 export default function Form() {
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
 
+    const formData = new FormData(e.target);
+    const password = formData.get("password");
+    const hashPassword = async (password: string): Promise<string> => {
+      const saltRounds = 10;
+      return bcrypt.hash(password, saltRounds);
+    };
+
+    let hashedPassword: string = "";
+    if (password && typeof password === "string") {
+      hashedPassword = await hashPassword(password);
+    }
     const data = {
       firstname: formData.get("firstname"),
       lastname: formData.get("lastname"),
       email: formData.get("email"),
-      password: formData.get("password"),
+      password: hashedPassword,
     };
-    // Ici, vous pouvez traiter ou envoyer les données du formulaire
-    console.log(data);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/user/create`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Problème lors de la connexion");
+      }
+      const responseData = await response.json();
+      console.log("Réponse de l'API", responseData);
+    } catch (error) {
+      console.error("Erreur lors de la connexion", error);
+    }
+  };
   };
 
   return (
