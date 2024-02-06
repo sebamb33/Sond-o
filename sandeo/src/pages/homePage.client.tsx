9a472ac583163aa845a7f5ead7824a0df4959827
Ambona Sebastien <sebastien.ambona@agence-gw.com>
2024-02-06 14:27:06 +0100

    homePage: work on component of navbar

---
+++ sandeo/src/pages/homePage.client.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import withAuth from "../app/hoc/withAuth.client";
+
+const HomePage = () => {
+  return (
+    <template>
+      <div className="homePageContent">
+        <h1>Je suis connecté</h1>
+      </div>
+    </template>
+  );
+};
+
+export default withAuth(HomePage);
