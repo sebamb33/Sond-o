20df645c095e1d93127b3204f94f257c8d0c6452
Ambona Sebastien <sebastien.ambona@agence-gw.com>
2024-02-13 10:55:05 +0100

    WIP now the navbar is make with daisyUi

--- sandeo/tailwind.config.ts
+++ sandeo/tailwind.config.ts
@@ -13,6 +13,23 @@ export default {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#1d4ed8",
+          "secondary": "#0ea5e9",
+          "accent": "#10b981",
+          "neutral": "#ffffff",
+          "base-100": "#ffffff",
+          "info": "#ffffff",
+          "success": "#84cc16",
+          "warning": "#ffffff",
+          "error": "#e11d48",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 } satisfies Config
 
