a769b3d757dfaef113f1e9d333ad0fd2358da605
Ambona Sebastien <sebastien.ambona@agence-gw.com>
2024-02-19 21:00:04 +0100

    Repair routes and now i can work on formulary

--- sandeo/src/app/components/templates/navbar.tsx
+++ sandeo/src/app/components/templates/navbar.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import { createAvatar } from "@dicebear/core";
-import { botttsNeutral } from '@dicebear/collection';
+import { botttsNeutral } from "@dicebear/collection";
 import "@/app/globals.css";
 import router from "next/router";
 import Cookies from "js-cookie";
@@ -31,46 +31,58 @@ const Navbar = ({ children }: NavbarProps) => {
     sessionStorage.removeItem("userData");
     Cookies.remove("token");
     router.push("/");
-  }
-  const avatarDataUrl = `data:image/svg+xml;base64,${btoa(avatar.toString())}`;
+  };
+  const avatarDataUrl = `data:image/svg+xml;base64,${btoa(avatar.wtoString())}`;
 
   return (
-      <div className="body__app">
-
-
+    <div className="body__app">
       <div className="navbar bg-base-100 ">
         <div className="flex-1">
-          <a className="btn btn-ghost text-3xl text-primary" onClick={() => router.push('/homePage')}>Sondéo</a>
+          <a
+            className="btn btn-ghost text-3xl text-primary"
+            onClick={() => router.push("/homePage")}
+          >
+            Sondéo
+          </a>
         </div>
         <div className="flex-none gap-2">
-            <ul className="menu menu-horizontal px-1">
-              <li><a>Sondages</a></li>
-              <li>
-               <a>Statistiques</a>
-              </li>
-            </ul>
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Sondages</a>
+            </li>
+            <li>
+              <a>Statistiques</a>
+            </li>
+          </ul>
           <div className="dropdown dropdown-end">
-
-            <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
+            <div
+              tabIndex="0"
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
               <div className="w-10 rounded-full">
-                <img alt="User Avatar" src={avatarDataUrl}/>
+                <img alt="User Avatar" src={avatarDataUrl} />
               </div>
             </div>
-            <ul tabIndex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <ul
+              tabIndex="0"
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
               <li>
-                <a className="justify-between">
-                  Mon profil ({userData?.mail})
-                </a>
+                <a className="justify-between">Mon profil ({userData?.mail})</a>
               </li>
-              <li><a>Parametres</a></li>
-              <li><a onClick={disconnect }>Déconnecter</a></li>
+              <li>
+                <a>Parametres</a>
+              </li>
+              <li>
+                <a onClick={disconnect}>Déconnecter</a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
-        {children}
-      </div>
-
+      {children}
+    </div>
   );
 };
 
--- sandeo/src/pages/formularyManage/[formularyid].tsx
+++ sandeo/src/pages/formularyManage/[formularyid].tsx
@@ -1,19 +1,71 @@
+import React, { useEffect, useState } from "react";
 import withAuth from "@/app/hoc/withAuth.client";
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 
 const FormularyManage = () => {
-    const router = useRouter();
-    const {formularyid} = router.query;
-    console.log(router.query);
-    let userAuthorized = false;
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const router = useRouter();
+  const { formularyid } = router.query;
 
+  const checkIfAuthorized = async () => {
+    try {
+      const token = Cookies.get("token");
+      const data = {
+        token: token,
+        formulary: formularyid,
+      };
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/formulary/checkIfAuthorized`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+        return responseData.authorized;
+      } else {
+        console.error("Problème lors de la vérification de l'autorisation");
+        return false;
+      }
+    } catch (error) {
+      console.error("Erreur lors de la vérification de l'autorisation", error);
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    if (formularyid) {
+      const fetchAuth = async () => {
+        const authorized = await checkIfAuthorized();
+        setIsAuthorized(authorized);
+      };
+
+      fetchAuth();
+    }
+  }, [formularyid]);
+
+  if (isAuthorized) {
     return (
-        <div>
-            <h1>FormularyManage</h1>
-            <p>{formularyid}</p>
-        </div>
+      <div>
+        <h1>FormularyManage</h1>
+        <p>{formularyid}</p>
+      </div>
     );
+  } else {
+    return (
+      <div>
+        <h1>FormularyManage</h1>
+        <p>Vous êtes pas autorisé à accéder à ce formulaire</p>
+      </div>
+    );
+  }
 };
-
 
 export default withAuth(FormularyManage);
