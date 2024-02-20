9c6d48ee5760c8009724d275c546f202fae8aef0
Ambona Sebastien <sebastien.ambona@agence-gw.com>
2024-02-20 22:27:22 +0100

    Transmit to component formularyID

--- sandeo/src/app/components/questions/manageQuestions.tsx
+++ sandeo/src/app/components/questions/manageQuestions.tsx
@@ -1,12 +1,30 @@
-export default function manageQuestions() {
+interface ManageQuestionsProps {
+    formularyID: number;
+}
+
+export default function ManageQuestions(props: ManageQuestionsProps) {
+
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      const {formularyID} = props;
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     try {
       const data = {
-        question: formData.get("question"),
+          question: formData.get("question") as string,
+          formularyID: formularyID,
       };
       //TODO make route for create question
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/question/create`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            }
+        );
     } catch (error) {}
   };
 
@@ -19,7 +37,7 @@ export default function manageQuestions() {
           placeholder="Question"
           className="input input-bordered w-full max-w-xs"
         />
-        <button className="btn btn-primary">Crée la questions</button>
+          <button className="btn btn-primary">Ajouter la questions</button>
       </form>
     </div>
   );
