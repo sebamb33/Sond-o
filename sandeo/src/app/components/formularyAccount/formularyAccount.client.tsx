3670febf729141069c145a3ca1b7359494a19ec6
Sébastien33am <71919598+sebamb33@users.noreply.github.com>
2024-03-01 23:13:33 +0100

    Add style and button for manage account

--- sandeo/src/app/components/formularyAccount/formularyAccount.client.tsx
+++ sandeo/src/app/components/formularyAccount/formularyAccount.client.tsx
@@ -1,28 +1,82 @@
+import {useState} from "react";
+
+type UserDataType = {
+    firstname: string;
+    lastname: string;
+    mail: string;
+};
+
+const getUserData = () => {
+    console.log(sessionStorage.getItem("userData"));
+    const userDataString = sessionStorage.getItem("userData");
+    if (userDataString) {
+        return JSON.parse(userDataString) as UserDataType;
+    }
+    const userDataEmpty: UserDataType = {} as UserDataType;
+    return userDataEmpty;
+};
+
+
 const FormularyAccountClient = () => {
+    const showModale = (e) => {
+        e.preventDefault();
+        setModaleVisible(true);
+    }
+    const closeModale = (e) => {
+        e.preventDefault();
+        setModaleVisible(false);
+    }
+    const [modaleVisible, setModaleVisible] = useState(false);
+    const [userData, setUserData] = useState<UserDataType | null>(getUserData());
     return (
-        <div className="flex-column justify-center">
-            <div className="flex-column justify-center">
-                <label className="form-control w-full max-w-xs">
+        <div className="w-full">
+            <form className={`flex-column w-1/2 m-auto ${modaleVisible ? 'blur-md' : ''}`}>
+                <label className="form-control w-full ">
                     <div className="label">
-                        <span className="label-text">Nom</span>
+                        <span className="label-text text-2xl text-primary">Nom</span>
                     </div>
                     <input type="text" placeholder="Nom de l'utilisateur"
-                           className="input input-bordered input-primary w-full max-w-xs"/>
+                           className="input input-bordered input-primary w-full " defaultValue={userData?.firstname}/>
                 </label>
-                <label className="form-control w-full max-w-xs">
+                <label className="form-control w-full ">
                     <div className="label">
-                        <span className="label-text">Prénom</span>
+                        <span className="label-text text-2xl text-primary">Prénom</span>
                     </div>
                     <input type="text" placeholder="Prénom de l'utilisateur "
-                           className="input input-bordered input-primary w-full max-w-xs"/>
+                           className="input input-bordered input-primary w-full " defaultValue={userData?.lastname}/>
                 </label>
-                <label className="form-control w-full max-w-xs">
+                <label className="form-control w-full ">
                     <div className="label">
-                        <span className="label-text">Mail</span>
+                        <span className="label-text text-2xl text-primary">Mail</span>
                     </div>
-                    <input type="text" placeholder="Mai de l'utilisateur "
-                           className="input input-bordered input-primary w-full max-w-xs"/>
+                    <input type="text" placeholder="Mail de l'utilisateur "
+                           className="input input-bordered input-primary w-full " defaultValue={userData?.mail}/>
                 </label>
+                <label className="form-control w-full ">
+                    <div className="label">
+                        <span className="label-text text-2xl text-primary">Nouveaux mot de passe:</span>
+                    </div>
+                    <input type="password" placeholder="mot de passe "
+                           className="input input-bordered input-primary w-full  "/>
+                </label>
+                <div className="flex gap-5 justify-end m-5">
+                    <button className="btn btn-primary">Modifier mes données</button>
+                    <button className="btn btn-accent" onClick={e => showModale(e)}>Supprimer mon compte
+                    </button>
+
+                </div>
+            </form>
+            <div
+                className={`deleteModale fixed  right-1/2 left-1/2 top-1/2 w-1/2 h-1/2 z-50  -translate-y-1/2 ${modaleVisible ? '' : 'hidden'}`}>
+                <div className="modal-box flex-col gap-3 w-full h-full justify-between">
+                    <h3 className="font-bold text-lg m-20">Voulez-vous vraiment supprimer votre compte ? </h3>
+                    <p className="text-primary m-10 text-center text-2xl text-accent">Cette action est irrémédiable </p>
+                    <div className="flex gap-3 justify-around  w-full">
+
+                        <button className="btn btn-primary " onClick={e => closeModale(e)}>Annuler</button>
+                        <button className="btn btn-accent ">Supprimer mon compte</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
